acaf5c6ae348590c134f3ecb1b59d704829bf288
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-04-06 11:51:15 -0300

    Home, Footer and Menu components were imported into App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './navegation/menu/menu.component';
+import { FooterComponent } from './navegation/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+    MenuComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
