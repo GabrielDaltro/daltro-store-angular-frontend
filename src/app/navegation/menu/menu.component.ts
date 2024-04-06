e3921bf358f3c7c1e7259c867b5c3f973cb02de0
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-04-06 11:43:09 -0300

    Home, Footer and Menu components were changed to standalone type

--- src/app/navegation/menu/menu.component.ts
+++ src/app/navegation/menu/menu.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-menu',
+    standalone: true,
     templateUrl: './menu.component.html'
 })
 export class MenuComponent{}
--- src/app/navegation/not-found/not-found.component.ts
+++ src/app/navegation/not-found/not-found.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-not-found',
+    standalone: true,
     templateUrl: './not-found.component.html'
 })
 export class NotFoundComponent{}
