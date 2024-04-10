bfa68cc9e7bb01d9b2b7f374f9dde96fdcafc711
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-04-10 11:45:57 -0300

    account.routes.ts was imported in main routes file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,5 +7,9 @@ export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'home', component: HomeComponent},
     {path:'products', component: ProductsComponent},
-    {path: 'suppliers', component: SuppliersComponent}
+    {path: 'suppliers', component: SuppliersComponent},
+    {
+        path: 'account',
+        loadChildren: () => import('./account/account.routes').then(feature => feature.AccountRoutes)
+    }
 ];
