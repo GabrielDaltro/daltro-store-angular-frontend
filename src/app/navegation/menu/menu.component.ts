7cdb4726987abf238237ae7e35a8a723857db509
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-04-12 10:36:33 -0300

    the css class seleced-menu-item is applied to active menu item

--- src/app/navegation/menu/menu.component.ts
+++ src/app/navegation/menu/menu.component.ts
@@ -1,11 +1,11 @@
 import { Component } from "@angular/core";
-import { RouterLink } from "@angular/router";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
     selector: 'app-menu',
     standalone: true,
     templateUrl: './menu.component.html',
     styleUrls: ['./menu.component.css'],
-    imports: [RouterLink]
+    imports: [RouterLink, RouterLinkActive]
 })
 export class MenuComponent{}
