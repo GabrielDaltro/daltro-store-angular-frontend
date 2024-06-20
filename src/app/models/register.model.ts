763f4cad05cbb8de900602775003e576d4bd76ac
GabrielDaltro <gabriel.d.8@hotmail.com>
2024-06-20 10:41:34 -0300

    created RegisterModel

---
+++ src/app/models/register.model.ts
@@ -0,0 +1,31 @@
+export class RegisterModel {
+
+    private readonly _name: string;
+    private readonly _email: string;
+    private readonly _password: string;
+    private readonly _confirmPassword: string;
+    
+
+    constructor(name: string, email: string, password: string, confirmPassword: string) {
+        this._name = name;
+        this._email = email;
+        this._password = password;
+        this._confirmPassword = confirmPassword;
+    }
+
+    get name() : string {
+        return this._name; 
+    }
+
+    get email() : string {
+        return this._email;
+    }
+
+    get password() : string {
+        return this._password;
+    }
+
+    get confirmPassword() {
+        return this._confirmPassword;
+    }
+}
